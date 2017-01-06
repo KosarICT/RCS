3fb7222ce32e13678b5cdba094104dcfbda2345f
Ali-Pc <ali@gmail.com>
2017-01-06 02:51:06 -0800

    version 0.0.3

--- src/main/java/com/kosarict/entity/ComplainantRelation.java
+++ src/main/java/com/kosarict/entity/ComplainantRelation.java
@@ -66,33 +66,6 @@ public class ComplainantRelation {
         this.nationalCode = nationalCode;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        ComplainantRelation that = (ComplainantRelation) o;
-
-        if (complainantRelationId != that.complainantRelationId) return false;
-        if (relationId != that.relationId) return false;
-        if (complainId != that.complainId) return false;
-        if (firstName != null ? !firstName.equals(that.firstName) : that.firstName != null) return false;
-        if (lastName != null ? !lastName.equals(that.lastName) : that.lastName != null) return false;
-        if (nationalCode != null ? !nationalCode.equals(that.nationalCode) : that.nationalCode != null) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = (int) (complainantRelationId ^ (complainantRelationId >>> 32));
-        result = 31 * result + (int) relationId;
-        result = 31 * result + (int) (complainId ^ (complainId >>> 32));
-        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
-        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
-        result = 31 * result + (nationalCode != null ? nationalCode.hashCode() : 0);
-        return result;
-    }
 
     @Basic
     @Column(name = "Ticket_Id")
